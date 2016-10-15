cb83a7f3b77673a38fbebbe1dbf1db121a2ca01f
tlk <guossnh@gmail.com>
2016-10-15 09:26:55 +0800

    taobao change add

--- sell_detail_copy/mingjun.js
+++ sell_detail_copy/mingjun.js
@@ -8,20 +8,21 @@
 //@include    https://buyertrade.taobao.com/trade/detail/trade_order_detail.htm*
 // @require  http://lib.sinaapp.com/js/jquery/1.7.2/jquery.min.js
 // ==/UserScript==
-//这是复制需要的信息成json格式 然后 粘贴进剪贴板的脚本
+//明君用的版本,她是两种excle格式
 
 //声明变量
 var wwid, sell_num, money, add, phone_num, transport_num, date, username, filename, pay_date; //旺旺ID , 单号 , 钱 , 地址 , 电话 , 运单号码,下单日期 ,姓名,付款日期
 var phone_num_exec = "^1[3|4|5|7|8][0-9]\d{8}$";
 var transport_num_exec = "\d{8,20}";
 
-
 function creat_button() {
     //往页面添加两个按钮用于复制两种信息
-    $("body").append("<a id='sell_ifo_copy' class='sell_ifo_copy' >卖</a>");
+    $("body").append("<a id='sell_ifo_copy' class='sell_ifo_copy' >卖1</a>");
+    $("body").append("<a id='sell_ifo_copy2' class='sell_ifo_copy2' >卖2</a>");
     //$("body").append("<a id='shua_ifo_copy' class='shua_ifo_copy' >刷</a>");
     //这是两个元素的css样式
     $("body").append("<style>.sell_ifo_copy {top:50px;left:10px;position:fixed;background-color:#44c767;-moz-border-radius:28px;-webkit-border-radius:28px;border-radius:28px;border:1px solid #18ab29;display:inline-block;cursor:pointer;color:#ffffff;font-family:Arial;font-size:17px;padding:16px 31px;    text-decoration:none;text-shadow:0px 1px 0px #2f6627;}.sell_ifo_copy:hover {background-color:#5cbf2a;}</style>");
+    $("body").append("<style>.sell_ifo_copy2 {top:100px;left:10px;position:fixed;background-color:#44c767;-moz-border-radius:28px;-webkit-border-radius:28px;border-radius:28px;border:1px solid #18ab29;display:inline-block;cursor:pointer;color:#ffffff;font-family:Arial;font-size:17px;padding:16px 31px;    text-decoration:none;text-shadow:0px 1px 0px #2f6627;}.sell_ifo_copy2:hover {background-color:#5cbf2a;}</style>");
     //$("body").append("<style>.shua_ifo_copy {top:150px;left:10px;position:fixed;background-color:#44c767;-moz-border-radius:28px;-webkit-border-radius:28px;border-radius:28px;border:1px solid #18ab29;display:inline-block;cursor:pointer;color:#ffffff;font-family:Arial;font-size:17px;padding:16px 31px;    text-decoration:none;text-shadow:0px 1px 0px #2f6627;}.shua_ifo_copy:hover {background-color:#5cbf2a;}</style>");
 }
 //2016年9月12日弃用这个寻找值的办法   狗日的淘宝 改版了
@@ -71,10 +72,22 @@ function get_value2() {
 
 creat_button();
 
+//购买日期/卖的盒数/空白/空白/空白/钱数/付款方式/邮件号/姓名地址电话都在一起/赠送的合数/旺旺号/
 $("#sell_ifo_copy").click(function() {
     get_value2();
     //copyToClipboard("[" + date + ",'',1," + wwid + "]");
-    $("body").append("<textarea style='font-family:Microsoft YaHei;font-size: 11px;text-align: center;' id = 'copy_text_id'><table><tr><td>" + date + "</td><td>" + filename + "</td><td>" + username + "</td><td>1</td><td>" + wwid + "</td><td>" + phone_num + "</td><td>" + add + "</td><td>" + sell_num + "</td><td>" + transport_num + "</td><td>" + money + "</td><td>T</td></tr></table></textarea>");
+    $("body").append("<textarea style='font-family:Microsoft YaHei;font-size: 11px;text-align: center;' id = 'copy_text_id'><table><tr><td>" + pay_date + "</td><td>1</td><td></td><td></td><td></td><td>" + money + "</td><td>淘宝</td><td>" + transport_num + "</td><td>" + add + "</td><td></td><td>" + wwid + "</td></tr></table></textarea>");
+    var copy_text_id = document.getElementById("copy_text_id");
+    copy_text_id.focus();
+    copy_text_id.select();
+    document.execCommand('Copy', false, null);
+});
+
+//姓名/地址/卖的瓶数/电话/下单时间/空白/付款方式/空白/赠送/钱数/邮件好/旺旺名字
+$("#sell_ifo_copy2").click(function() {
+    get_value2();
+    //copyToClipboard("[" + date + ",'',1," + wwid + "]");
+    $("body").append("<textarea style='font-family:Microsoft YaHei;font-size: 11px;text-align: center;' id = 'copy_text_id'><table><tr><td>" + username + "</td><td>" + add + "</td><td>1</td><td>" + phone_num + "</td><td>" + pay_date + "</td><td></td><td>淘宝</td><td></td><td></td><td>" + money + "</td><td>" + transport_num + "</td><td>" + wwid + "</td></tr></table></textarea>");
     var copy_text_id = document.getElementById("copy_text_id");
     copy_text_id.focus();
     copy_text_id.select();
