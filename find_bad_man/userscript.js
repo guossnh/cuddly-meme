97b4ef0eee6514ae63c3b1237fe051c9074e8313
guossnh <guossnh@gmail.com>
2017-07-28 18:03:23 +0800

    我干 js的异步编程

--- find_bad_man/userscript.js
+++ find_bad_man/userscript.js
@@ -8,33 +8,52 @@
 // @require  http://lib.sinaapp.com/js/jquery/1.7.2/jquery.min.js
 // ==/UserScript==
 
-(function() {
-    setTimeout(function(){
-        //$("a:contains('详情')").html("wocoa");
-        console.log($("a:contains('详情')").length);
-        console.log($("a:contains('详情')").attr("href"));
-        alert($("a:contains('详情')").get()[1]);
-    },1000);
-})();
 //全局变量区域
-
-//获取所有的连接并且找到订单号码放入map
-    function doList(){
-        return true;
-    }
+var urllist = new Array(0);
+var keyWorld = new Array("极限品牌2015","鹏哥到此一游","啥尼英伦会社","张恒","罗腾","18005651225","15375371225","15395511780","北干街道建设2路","红十燕七里桥","龙池办事处","麻城市","萧山区","贤武","唐倩");
+var doUrlList = new Array(0);
+//处理页面上边的详情连接并且放入urllist
+    function doList (){
+        var data = $("a:contains('详情')");
+        var datas;
+        console.log("dolist have dane");
+        for(var i = 0;i<data.length;i++){
+            datas = data.get()[i].href.toString();
+            if (datas.indexOf("detail")===31){
+                urllist.push(datas);
+            }
+        }
+}
 //根据订单修改连接判断这个的连接的页面是否出现差评时的特征返回Ture或者False
-    function findBadManKeyWorld(link){
-        //示例  这样可以 异步获取订单详细信息
-        $.get("https://trade.taobao.com/trade/detail/trade_order_detail.htm?biz_order_id=36951328894888290", function(result){
-            alert(request.indexOf("taobao"))
+    function findBadManKeyWorld (link){
+        $.get(link, function(result){
+            for (var i = 0; i<keyWorld.length;i++){
+                console.log("寻找关键词执行"+i+"次");
+                //console.log(result.indexOf(keyWorld[i]));
+                if(result.indexOf(keyWorld[i])>0){
+                    console.log("我草一次");
+                    doUrlList.push(link);
+                    break;
+                }
+            }
           });
-        return true;
     }
-//新建list存入值为Ture的订单号,并且清空map的值
+//新建list存入值为Ture的订单号
     function getTheList(){
-
+        console.log("getTheList");
+        for(var i = 0; i<urllist.length;i++){
+            findBadManKeyWorld(urllist[i]);
+        }
     }
 //通过list里边的值来渲染页面的订单内容
     function doSomeThing(){
-
+        setTimeout(function(){
+            console.log("dosomething"+doUrlList.length);
+        });
     }
+//页面载入之后执行
+(function() {
+    setTimeout(function(){
+        
+    },1000);
+})();
