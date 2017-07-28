97f7960a853fe566d9ce70b85ce27913abaf6d67
guossnh <guossnh@gmail.com>
2017-07-28 08:16:17 +0800

    111

---
+++ find_bad_man/userscript.js
@@ -0,0 +1,6 @@
+ 
+//示例  这样可以 异步获取订单详细信息
+$.get("https://trade.taobao.com/trade/detail/trade_order_detail.htm?biz_order_id=36951328894888290", function(result){
+    alert(request.indexOf("taobao"))
+  });
+
