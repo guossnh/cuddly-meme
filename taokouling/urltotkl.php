eac63ffda156d7cbaf544c6cf2cff31c573882c7
guossnh <guossnh@gmail.com>
2018-03-09 12:43:07 +0800

    修改最后两个问题,现在程序可以完整的跑完了

--- taokouling/urltotkl.php
+++ taokouling/urltotkl.php
@@ -4,17 +4,33 @@
     $appkey = "24813912";
     $secret = "60afb8c12e2e930d454422cadb09ab3f";
 
+    $babyid = $_POST['babyid'];
+    $babynum = $_POST['babynum'];
+
+
+
     $c = new TopClient;
     $c->appkey = $appkey;
     $c->secretKey = $secret;
     $c->format = "json";
-    $req = new TbkTpwdCreateRequest;
-    $req->setText("郭文卓真帅");
-    $req->setUrl("http://h5.m.taobao.com/awp/core/detail.htm?locate=guessitem-item&spm=a2141.1.guessitem.guessitem-"+$_POST['linknum']+"&scm=1007.12286.76309.110&id="+$_POST['shopid']+"");
-    $req->setLogo("https://uland.taobao.com/");
-    $req->setExt("{}");
+    //$req = new TbkTpwdCreateRequest;
+    //$req->setText("郭文卓真帅");
+    //$req->setUrl("https://h5.m.taobao.com/awp/core/detail.htm?locate=guessitem-item&spm=a2141.1.guessitem.guessitem-$babynum&//scm=1007.12286.76309.110&id=$babyid");
+    //$req->setLogo("https://uland.taobao.com/");
+    //$req->setExt("{}");
+    //$resp = $c->execute($req);
+    //echo $resp->data->model;
+    $req = new WirelessShareTpwdCreateRequest;
+    $tpwd_param = new GenPwdIsvParamDto;
+    $tpwd_param->ext="{\"xx\":\"xx\"}";
+    $tpwd_param->logo="https://uland.taobao.com/";
+    $tpwd_param->url="https://h5.m.taobao.com/awp/core/detail.htm?locate=guessitem-item&spm=a2141.1.guessitem.guessitem-$babynum&//scm=1007.12286.76309.110&id=$babyid";
+    $tpwd_param->text="超值活动，惊喜活动多多";
+    $tpwd_param->user_id="12312312";
+    $req->setTpwdParam(json_encode($tpwd_param));
     $resp = $c->execute($req);
-    echo $resp->data->model;
-    //SimpleXMLElement Object ( [data] => SimpleXMLElement Object ( [model] => ￥MznT0LTwEk6￥ ) [request_id] => 11ifxh5tsh5cx )
-    //stdClass Object ( [data] => stdClass Object ( [model] => ￥a3rz0LTDN8s￥ ) [request_id] => iv1pugh4xwe7 )
+    echo $resp->model;
+    //print_r($resp);
+    
+
 ?>
