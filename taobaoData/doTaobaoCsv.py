e3e4fc7fa3aaba687f4c44a687a7546ff9f110e4
guossnh <guossnh@gmail.com>
2018-04-04 19:12:33 +0800

    先添加

---
+++ taobaoData/doTaobaoCsv.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+import os
+
+csvAddress = "c:\\csvCache\\"
+csvFileList = []
+
+class doTaobaoCsv(object):
+
+    def __init__(self,csvAddress):
+        pass
+    #这个方法是读取csv数据并且对数据进行分析
+    def readcsv(self):
+        global csvFileList
+        for filelink in csvFileList:
+            print(filelink)
+    #获取该文件夹下边的所有csv文件名称放入list
+    def getfile(self):
+        global csvAddress,csvFileList
+        for file in os.listdir(csvAddress):
+            csvFileList.append(csvAddress+file)
+        print(csvFileList)
+
+    if __name__ == '__main__':
+        getfile(1)
+        readcsv(1)
