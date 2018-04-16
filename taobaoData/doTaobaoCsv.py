05db3b2f793e2304982fd039724eea1e6b87636c
guossnh <guossnh@gmail.com>
2018-04-16 11:19:41 +0800

    更新一下

--- taobaoData/doTaobaoCsv.py
+++ taobaoData/doTaobaoCsv.py
@@ -8,8 +8,7 @@ orderVOList =[]#这是产品对象的列表
 reWeixin = re.compile(r'(G-){1}[A-Z]{2,4}-{1}([A-Z]{1,5}\*{0,1}\d{0,2}\+{0,1})+\({1}\d{1,3}\+{1}\d{1,3}\){1}')
 reWangzhan = re.compile(r'(V-){1}[A-Z]{2,4}-{1}([A-Z]{1,5}\*?\d{0,2}\+?)+')
 reZhengchang = re.compile(r'[A-Z]{2,4}-{1}([A-Z]{1,5}\*?\d{0,2}\+?)+')
-
-
+optputFile = "c:\\csvCache\\result.txt"
 
 #这个方法是读取csv数据并且对数据进行分析
 def readcsv():
@@ -22,8 +21,8 @@ def readcsv():
                 if (row[10].find('已')>-1):
                     doTitle(row[19])
                     for ov in orderVOList:
-                        if (ov.name == row[19]):
-                            analysis(ov,row[23],float(row[7]))
+                        if (ov.babyName == row[19]):
+                            analysis(ov,row[23],row[8],row[0])
 
 def doTitle(title):
     global orderVOList
@@ -37,27 +36,46 @@ def getfile():
     global csvAddress,csvFileList
     for file in os.listdir(csvAddress):
         csvFileList.append(csvAddress+file)
-    print(csvFileList)
 
     #G-GWZ-MJ*1(100+5)-ST   这些是标记范例
     #GWZ-PJW*1+JCF*2-BS
     #V-GWZ-PJW*1-YD
-def analysis(ov,shellReadmark,money):
+def analysis(ov,shellReadmark,money,sellID):
+    shellReadmark = shellReadmark.lstrip("'")
     if(shellReadmark.find('null')!=-1):#判断是不是没有填写备注
-        ov.unknowRemarkMoney+=money
-        ov.unknowRemarkNum+=1
+        ov.noInformation(money)
     elif(reWeixin.match(shellReadmark)):#判断是不是微信刷单  
-        ov.babyWeixinMoney+=money
-        ov.babyWeixinNum+=1
+        weixinre = shellReadmark.split('(')[1].split(')')[0].split('+')
+        ov.weixin(money,weixinre[0],weixinre[1])
     elif(reWangzhan.match(shellReadmark)):#判断是不是网站刷单  
-        print('网站刷单')
-    elif(reZhengchang.match(shellReadmark)):#判断是真实刷单 
-        print('正常单子')
+        ov.web(money)
+    elif(reZhengchang.match(shellReadmark)):#判断是真实单子
+        ov.trueSell(money)
     else:
-        print('错误格式')
+        ov.wrongr(money,sellID)
+
+
+def output():#输出文本文档
+    global optputFile ,orderVOList
+    with open(optputFile,"w") as f:
+        for ov in orderVOList:
+            f.write(
+            """
+            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~华丽的分割线~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n
+            产品标题:"""+str(ov.babyName)+"""\n
+            真实销量:"""+str(ov.babysellMoney)+"""总数量:"""+str(ov.babysellNum)+"""\n
+            网站刷单:"""+str(ov.babyWebMoney)+"""总数量:"""+str(ov.babyWebNum)+"""\n
+            微信刷单:"""+str(ov.babyWeixinMoney)+"""总数量:"""+str(ov.babyWeixinNum)+"""微信备注总额:"""+str(ov.weixinRemarkMoney)+"""佣金总额:"""+str(ov.weixinRemarkMoney2)+"""\n
+            没有备注:"""+str(ov.noRemarkMoney)+"""总数量:"""+str(ov.noRemarkNum)+"""\n
+            备注不对:"""+str(ov.unknowRemarkMoney)+"""总数量:"""+str(ov.unknowRemarkNum)+"""\n
+            备注错误单号:"""+str(ov.worongSellId)+"""
+            """
+            )
 
 if __name__ == '__main__':
     getfile()
     readcsv()
-    #analysis('GWZ-MJ*1+LJD*2')
+    output()
+
+
 
--- taobaoData/orderVO.py
+++ taobaoData/orderVO.py
@@ -1,37 +1,47 @@
 # -*- coding: UTF-8 -*-
 
+from decimal import Decimal
 #这是产品对象的实体类,因为是根据交易信息的标题标识的,所以可能有出错的地方.暂时的解决方法
 
 class orderVO(object):
     babyName = ""#产品名称
-    sellId = []#交易ID
-    babyWeixinMoney = 0#微信销售金额
-    babyWeixinNum = 0#微信销售数量
-    weixinRemarkMoney =0#微信标记数量
-    weixinReakNum =0#微信标记数量
-    babysellMoney0 = 0#真实销售本金
-    babysellMoney1 = 0#真实销售佣金
+    worongSellId = []#交易ID
+    babyWeixinMoney = '0'#微信销售金额
+    babyWeixinNum = 0#微信刷单数量
+    weixinRemarkMoney ='0'#微信刷单总金额
+    weixinRemarkMoney2 = '0'#微信刷单总佣金
+    babysellMoney = '0'#真实销售
     babysellNum = 0#真实销售数量
-    babyWebMoney = 0#网站放单金额
+    babyWebMoney = '0'#网站放单金额
     babyWebNum =0#网站放单数量
-    noRemarkMoney =0#没有标记的总金额
+    noRemarkMoney ='0'#没有标记的总金额
     noRemarkNum =0#没有标记的总数量
-    unknowRemarkMoney =0#没有识别标记的总金额
+    unknowRemarkMoney ='0'#没有识别标记的总金额
     unknowRemarkNum =0#没有识别标记的总数量
 
     def __init__(self,name):
         self.babyName = name
+        self.worongSellId=[]
 
+    def weixin(self,money,rmoney,rmoney2):#微信
+        self.babyWeixinMoney = Decimal(str(self.babyWeixinMoney)) + Decimal(str(money))
+        self.babyWeixinNum = self.babyWeixinNum + 1
+        self.weixinRemarkMoney = Decimal(str(self.weixinRemarkMoney)) + Decimal(str(rmoney))
+        self.weixinRemarkMoney2 = Decimal(str(self.weixinRemarkMoney2)) + Decimal(str(rmoney2))
 
-    def addOneData(self,sellRemark,sellId):
-        self.sellId.append(sellId)
-    
-    def weixin(self,money,num,rmoney,rnum):
-        pass
-    def noin(self,money,num):
-        pass
-    def web(self,money,num):
-        pass
-    def wrongr(self,money,num):
-        pass
-    
+    def noInformation(self,money):#没有备注
+        self.noRemarkMoney = Decimal(str(self.noRemarkMoney)) + Decimal(str(money))
+        self.noRemarkNum = self.noRemarkNum + 1
+        
+    def web(self,money):#网站放单
+        self.babyWebMoney = Decimal(str(self.babyWebMoney)) + Decimal(str(money))
+        self.babyWebNum = self.babyWebNum + 1
+        
+    def wrongr(self,money,sellID):#错误标记
+        self.unknowRemarkMoney = Decimal(str(self.unknowRemarkMoney)) + Decimal(str(money))
+        self.unknowRemarkNum = self.unknowRemarkNum + 1
+        self.worongSellId.append(sellID)
+
+    def trueSell(self,money):#真实销量
+        self.babysellMoney = Decimal(str(self.babysellMoney)) + Decimal(str(money))
+        self.babysellNum = self.babysellNum + 1
