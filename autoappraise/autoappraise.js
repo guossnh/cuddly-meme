df6f61a428a38788d67798065cfe5e8466e58f7f
guossnh <guossnh@gmail.com>
2019-05-14 15:33:20 +0800

    加一个快速评价的文件

--- autoappraise/autoappraise.js
+++ autoappraise/autoappraise.js
@@ -8,16 +8,17 @@
 // @grant        none
 // ==/UserScript==
 
+//定义提交方法，貌似不分开写的话总是不能提交
 function summitbutton(){
     document.getElementsByClassName("J_btn_submit tb-rate-btn type-primary wide-xxl")[0].click()
 }
-
+//这是页面打开之后先要给点击好评，然后再延时2秒提交表单
 (function() {
     document.getElementById("rate-good-all").click();
     document.getElementsByClassName("good-rate")[0].click()
-    //document.getElementsByClassName("J_btn_submit tb-rate-btn type-primary wide-xxl")[0].click()
     setTimeout(summitbutton(), 2000);
 })();
+//这个再chrome里边没法关闭页面 想想通过按下x按键可以试试
 
 
 
